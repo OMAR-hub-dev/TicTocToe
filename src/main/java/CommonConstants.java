cd14edbc73eb2f8551ece949f9596978209089c0
Oulaya omar <oulaya.omar@gmail.com>
2023-11-11 00:18:29 +0100

    config

---
+++ src/main/java/CommonConstants.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class CommonConstants {
+//    size config
+    public static final Dimension FRAME_SIZE = new Dimension(540,760);
+//    color config
+    public static final Color background_color = Color.decode("#011627");
+    public static final Color x_color = Color.decode("#E71D36");
+    public static final Color o_color = Color.decode("#FF9F1C");
+    public static final Color bar_color = Color.decode("#2Ec486");
+    public static final Color board_color = Color.decode("#FDFFFC");
+}
---
+++ src/main/java/TicTocToe.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class TicTocToe extends JFrame {
+    public TicTocToe(){
+        super("Tic Toc Toe (java swing)");
+        setSize(CommonConstants.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        addGuiComponement();
+    }
+    private void addGuiComponement(){
+
+    }
+}
